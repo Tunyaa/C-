b80ad10d6202aa9d0ce4a59a8150915090bbc1aa
oldca <oldca@DESKTOP-B5D2JS2>
2018-11-14 12:34:40 +0300

    11

---
+++ hot/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        
+            Random rnd = new Random();
+            int value = rnd.Next(0, 100);
+            
+            Console.WriteLine("Введите ваше число:");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            //Console.WriteLine($"{value}"); проверка сгенерированного числа
+            while (num != value)
+            {
+                while (num < value)
+                {
+                    Console.WriteLine("Холодно");
+                    num = Convert.ToInt32(Console.ReadLine());
+
+                }
+
+                while (num > value)
+                {
+                    Console.WriteLine("Горячо");
+                    num = Convert.ToInt32(Console.ReadLine());
+
+                }
+            }
+
+
+
+            Console.WriteLine("Успех");
+            
+            Console.ReadLine();
+
+            
+        }
+    }
+}
